fc308bce266cada13115bb8efaa46db860ddd3eb
realBrando <89408113+realBrando@users.noreply.github.com>
2022-04-16 17:56:26 -0400

    Create Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Teacher
+ *
+ * Teacher class
+ *
+ * @author Luca Marinello, Andrew Brandon, Alexandre Moraes, Alice Dinh, Eric Yong
+ *
+ * @version April 11, 2022
+ *
+ */
+public class Teacher extends Account {
+
+    public Teacher(String username, String password) {
+        super(username, password);
+    }
+
+}
