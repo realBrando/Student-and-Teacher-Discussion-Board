0fc961139a8bd5e8adf957841c2b1c22b8b2e400
realBrando <89408113+realBrando@users.noreply.github.com>
2022-04-16 17:55:55 -0400

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,17 @@
+/**
+ * Student
+ *
+ * This program is a subclass of Account for people who log in as students.
+ *
+ * @author Luca Marinello, Andrew Brandon, Alexandre Moraes, Alice Dinh, Eric Yong
+ *
+ * @version April 11, 2022
+ *
+ */
+public class Student extends Account {
+
+    public Student(String username, String password) {
+        super(username, password);
+    }
+
+}
