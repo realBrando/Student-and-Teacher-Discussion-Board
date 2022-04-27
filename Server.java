a0cb77ed88be62da143f121ccded98419cb962ed
Luca <44023561+Casheh@users.noreply.github.com>
2022-04-27 12:40:30 -0400

    Updated Server.java to allow for concurrency

--- Server.java
+++ Server.java
@@ -2,20 +2,35 @@ import java.io.*;
 import java.net.*;
 import javax.swing.*;
 
-public class Server {
-    public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(4242);
-        waitingToConnect();
-        Socket socket = serverSocket.accept();
-        connected();
-    }
-    public static void waitingToConnect() {
-        JOptionPane.showMessageDialog(null, "Client has not Connected",
-                "SERVERIO", JOptionPane.INFORMATION_MESSAGE);
-    }
+public class Server implements Runnable {
 
-    public static void connected() {
-        JOptionPane.showMessageDialog(null, "Client Connected",
-                "SERVERIO", JOptionPane.INFORMATION_MESSAGE);
-    }
+    private Socket socket;
+
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(4242);
+        waitingToConnect();
+        while (true) {
+            Socket socket = serverSocket.accept();
+            connected();
+            new Thread(new Server(socket)).start();
+        }
+
+    }
+    public static void waitingToConnect() {
+        JOptionPane.showMessageDialog(null, "Client has not Connected",
+                "SERVERIO", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void connected() {
+        JOptionPane.showMessageDialog(null, "Client Connected",
+                "SERVERIO", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public void run() {
+
+    }
 }
