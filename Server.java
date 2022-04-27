6676944d11707c22116ead98dc2b511a2f7cf5d9
Luca <44023561+Casheh@users.noreply.github.com>
2022-04-27 15:53:44 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -12,11 +12,12 @@ public class Server implements Runnable {
 
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(4242);
-        waitingToConnect();
         while (true) {
             Socket socket = serverSocket.accept();
-            connected();
-            new Thread(new Server(socket)).start();
+            Thread thread = new Thread(new Server(socket));
+            thread.start();
+            thread.join();
+
         }
 
     }
