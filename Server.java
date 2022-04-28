e8c31d9259ff2b8fba6bde41d4589ee88d5c4b2f
Luca <44023561+Casheh@users.noreply.github.com>
2022-04-28 10:43:40 -0400

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,10 +1,15 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Server implements Runnable {
 
     private Socket socket;
+
+    public static ArrayList<Account> accounts = new ArrayList<>();
+
+    public static ArrayList<Course> courses = new ArrayList<>();
 
     public Server(Socket socket) {
         this.socket = socket;
@@ -16,6 +21,12 @@ public class Server implements Runnable {
             Socket socket = serverSocket.accept();
             Thread thread = new Thread(new Server(socket));
             thread.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+            // Read in string from client (DicussionBoard2)
+
             thread.join();
 
         }
